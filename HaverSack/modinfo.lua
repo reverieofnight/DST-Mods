767a514066ca52bd7c41e64b124d6474bca68b17
YangHai <120018091@qq.com>
2026-07-05 10:47:20 +0800

    fix(HaverSack): 更新模组图标文件引用
    替换旧的bigbagicon图标引用为modicon，删除旧图标文件并添加新预览图和tex文件

--- HaverSack/modinfo.lua
+++ HaverSack/modinfo.lua
@@ -23,8 +23,8 @@ client_only_mod = false
 server_filter_tags = {"Haversack", "RainyNight", "nicebigbag"}
 
 forumthread = ""
-icon_atlas = "haversackicon.xml"
-icon = "haversackicon.tex"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 
 priority = 0
 configuration_options = {
